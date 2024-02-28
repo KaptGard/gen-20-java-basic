eb8d66e82b811323bb94f1953188f7edb21896e0
Gardenia Marheta <gardeniamarheta@gmail.com>
2024-02-28 10:36:38 +0700

    gen-20-java-basic-new

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Nama : Gardenia Marheta Putra
+// Ini merupakan perocbaan pertama pemrograman java untuk menampilkan tulisan Hello World!
+
+// Pertama kita harus menyiapkan kerangka untuk java yaitu class
+// Untuk class harus sesuai dengan nama program yang kita jalankan atau kita buat, misalnya bernama perhitungan.java
+// maka nama class juga harus sesuai, disini saya menggunakan Main.Java maka menggunakan class Main
+class Main {
+    // setelah class kita harus menyertakan atau deklarasi void main dengan string seperti berikut
+    // untuk
+    public static void main(String[] args) {
+        // Disini kita mulai mengetikan kodingan atau syntak yang kita ingin buat, dimulai dari buka kurung kurawal setelah deklarasi void
+        System.out.printf("Hello World!");
+        // fungsi seperti diatas adalah untuk menampilkan keluar atau out, dan fungsi printf adalah untuk menampilkan kata atau variabel yang ada dialam kurung
+        // Print untuk sekedar menampilkan kata
+        // printf untuk menampilkan kata atau variabel
+        // println untuk menampilkan kata dengan baris baru atau paragraf baru
+    }
+}
+// setiap program java ditutup dengan kurung kurawal, setiap syntax ditutup dengan ; seperti pada bahasa C
